454952127218ce29c9a31f51c72487c886c0162d
Joseph Coleman <sirusxcoder@gmail.com>
2023-06-28 14:08:59 -0700

    Publish Index page working

--- app/publish/views.py
+++ app/publish/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    """ Render the landing page for publish """
+    return render(request, 'publish/index.html')
